5e4a51731c23992d350b8e10ad85c9e1ed2f27b5
Sachin V S <vs1sachi12@gmail.com>
2026-02-09 13:10:58 +0530

    committ4

--- controllers/authcontrollers.js
+++ controllers/authcontrollers.js
@@ -40,3 +40,103 @@ exports.admin = asyncHandler(async (req, res, next) => {
     token,
   });
 });
+
+// login
+exports.admin = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await user.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const isMatch = await verifypass(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const token = await user.getToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+//admin
+exports.admin = asyncHandler(async (req, res, next) => {
+  const { name, email } = req.body;
+  const user = await user.findOne({ email }).select("+password");
+  if (!this.admin) {
+    return next(new ErrorResponse("Invalid credential", 401));
+  }
+  const isMatch = await verifypass(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  const token = await this.admin.getToken();
+  res.status(200).json({
+    success: true,
+    token,
+  });
+});
+
+const company = asyncHandler(async (req, res, next) => {
+  const { name, email, password, label, address } = req.body;
+  const company = await company.findone(name);
+  if (!company) {
+    return next(new ErrorResponse("Company alreday exists", 409));
+  }
+  const newcompany = new company({ name, email, password, label, address });
+  await newcompany.save();
+  res.status(200).json({
+    success: true,
+    data: newcompany,
+  });
+});
+
+// get company
+exports.getcompany = asyncHandler(async (req, res, next) => {
+  const { companyId } = req.params;
+  const company = await company.findById(companyId);
+  if (!company) {
+    return next(
+      new ErrorResponse(`no found with company by id${companyId}`, 409),
+    );
+  }
+  res.status(200).json({
+    success: true,
+    data: company,
+  });
+});
+
+// delete company
+exports.deleteCompany = asyncHandler(async (req, res, next) => {
+  const { id } = req.params;
+  const company = await company.findById(id);
+  if (!company) {
+    return next(new ErrorResponse(`no comapny found with id${id}`, 404));
+  }
+  await company.deleteone();
+  res.status(200).json({
+    success: true,
+    data: [],
+  });
+});
+
+//managerlogin
+exports.loginAsmanager = asyncHandler(async (req, res, next) => {
+  const { email, password } = req.body;
+  const user = await Manager.findOne({ email })
+    .select("+password")
+    .populate("company");
+  if (!user) {
+    return next(new ErrorResponse("Invalid credentials", 401));
+  }
+  const isMatch = await user.verifypass(password);
+  if (!isMatch) {
+    return next(new ErrorResponse("Invalid Credentials", 401));
+  }
+  const token = await user.getToken();
+  res.status(200).json({
+    success: true,
+    user,
+    token,
+  });
+});
